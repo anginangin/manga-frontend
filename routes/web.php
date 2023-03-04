b21c55ff116da38c0522040774437cf8aec4f514
Raul <raulirawan33@gmail.com>
2023-03-04 15:11:01 +0700

    exec shell

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::get('shell-exec', function() {
+    chdir('..');
+    shell_exec('php artisan queue:work --stop-when-empty >> /dev/null 2>/dev/null &');
+});
 Route::get('/mangajob', [DetailController::class, 'mangajob']);
 Route::get('/config-cache', function () {
     \Artisan::call('config:cache');
