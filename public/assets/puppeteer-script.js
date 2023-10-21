3e9214cd05a99dac5f833e8fdf554138ba68bada
raulirawan <raulirawan33@gmail.com>
2023-10-21 15:43:07 +0700

    gas

--- public/assets/puppeteer-script.js
+++ public/assets/puppeteer-script.js
@@ -5,14 +5,12 @@ const url = process.argv[2]; // URL passed as a command line argument
 (async () => {
   const browser = await puppeteer.launch({
     headless: true, // Set to false for non-headless mode
-    executablePath: '/usr/bin/chromium-browser',
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    defaultViewport: null,
-    ignoreHTTPSErrors: true
   });
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
   await page.waitForSelector('.clstyle');
   // Evaluate JavaScript function to filter DOM elements
   const filteredData = await page.evaluate(() => {
