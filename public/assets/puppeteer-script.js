17231d2b519fbc26019db7e88a8e79a56b48bc0d
raulirawan <raulirawan33@gmail.com>
2023-10-21 15:23:24 +0700

    Gas

--- public/assets/puppeteer-script.js
+++ public/assets/puppeteer-script.js
@@ -10,7 +10,7 @@ const url = process.argv[2]; // URL passed as a command line argument
   const page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'domcontentloaded' });
-
+  await page.waitForSelector('body');
   // Evaluate JavaScript function to filter DOM elements
   const filteredData = await page.evaluate(() => {
     // Custom filtering logic, for example, selecting all <a> elements with a specific class
