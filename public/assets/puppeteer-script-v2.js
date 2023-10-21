7ae8514ce06a14f44da31593ad0c7034fb682439
raulirawan <raulirawan33@gmail.com>
2023-10-21 13:56:22 +0700

    Gas

---
+++ public/assets/puppeteer-script-v2.js
@@ -0,0 +1,29 @@
+const puppeteer = require('./js/puppeteer/lib/types');
+
+const url = process.argv[2]; // URL passed as a command line argument
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+  // Evaluate JavaScript function to filter DOM elements
+  const filteredData = await page.evaluate(() => {
+    // Custom filtering logic, for example, selecting all <a> elements with a specific class
+    const elements = Array.from(document.querySelectorAll('.clstyle li'));
+    const filteredElements = elements.map(element => {
+      return {
+        cp: element.querySelector('.chapternum').textContent,
+        url: element.querySelector('a').href
+      };
+    });
+    return filteredElements;
+  });
+
+  console.log(JSON.stringify({
+    data: filteredData
+  }));
+
+  await browser.close();
+})();
